023754087fc75548752b3c492310fa23c15f4cb2
MrFermz <Mr.Fermz@hotmail.com>
2020-04-23 19:32:12 +0700

    new file

---
+++ resume/data/content.js
@@ -0,0 +1,86 @@
+
+// Info
+const INFO = {
+    content :   [
+                    [`title`, `detail`],
+                    [`title`, `detail`],
+                    [`title`, `detail`],
+                    [`title`, `detail`],
+                ]
+}
+
+
+
+
+// Home card
+const ABOUT = {
+    title   : `about me`,
+    content : `Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the first main entry in the Grand Theft Auto series since 2008's Grand Theft Auto IV. Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three criminals and their efforts to commit heists while under pressure from a government agency and powerful crime figures. The open world design lets players freely roam San Andreas' open countryside and the fictional city of Los Santos, based on Los Angeles.`
+}
+
+const HOBBY = {
+    title   :   `hobbies`,
+    content :   [
+                    [`work`, `title1`, `countryside and the fictional city of Los Santos, based on Los Angeles.`],
+                    [`work`, `title2`, `countryside and the fictional city of Los Santos, based on Los Angeles.`],
+                    [`work`, `title3`, `countryside and the fictional city of Los Santos, based on Los Angeles.`],
+                ]
+}
+
+const SKILLS = {
+    title_design        :   `design skills`,
+    content_design      :   [
+                                [`content_design 1`, 40],
+                                [`content_design 4`, 75],
+                            ],
+    title_coding        :   `coding skills`,
+    content_coding      :   [
+                                [`content_coding 1`, 50],
+                                [`content_coding 3`, 60],
+                                [`content_coding 4`, 80],
+                            ]
+}
+
+
+
+// Resume card
+const RESUME = {
+    title               : `resume`
+}
+
+const TIMELINE = {
+    working_title   :   [
+                            [`working_title 1`, `2020`, `The single-player mode lets players control three characters`],
+                            [`working_title 2`, `2021`, `Each character has a set of eight skills that represent their`],
+                        ],
+    education_title :   [
+                            [`education_title 1`, `2020`, `The single-player mode lets players control three characters`],
+                            [`education_title 2`, `2021`, `Each character has a set of eight skills that represent their`],
+                            [`education_title 3`, `2022`, `While free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage inWhile free-roaming the game world, players may engage in`],
+                            [`education_title 3`, `2022`, `While free-roaming the game world, players may engage in`],
+                        ]
+}
+
+
+
+// Portfolio card
+const PORT = {
+    title   : `portfolio`,
+}
+
+const PORT_CONTENT = {
+    port_card   :   [
+                        [`port.jpg`, `title`, `short_detail`],
+                        [`port.jpg`, `title`, `short_detail`],
+                        [`port.jpg`, `title`, `short_detail`],
+                        [`port.jpg`, `title`, `short_detail`],
+                        [`port.jpg`, `title`, `short_detail`],
+                    ]
+}
+
+
+// Contact card
+const CONTACT = {
+    title   : `contact`,
+    content : `Players use melee attacks, firearms and explosives to fight enemies,[d] and may run, jump, swim or use vehicles to navigate the world.[e] To accommodate the map's size, the game introduces vehicle types absent in its predecessor Grand Theft Auto IV, such as fixed-wing aircraft.[11] In combat, auto-aim and a cover system may be used as assistance against enemies.[12] Should players take damage, their health meter will gradually regenerate to its halfway point.[f] Players respawn at hospitals when their health depletes.[10] If players commit crimes, law enforcement agencies may respond as indicated by a "wanted" meter in the head-up display (HUD).[13] Stars displayed on the meter indicate the current wanted level (for example, at the maximum five-star level, police helicopters and SWAT teams swarm to lethally dispatch players).[14][g] Law enforcement officers will search for players who leave the wanted vicinity. The meter enters a cool-down mode and eventually recedes when players are hidden from the officers' line of sight that displays on the mini-map.[16][h]`
+}
---
+++ resume/functions/libs.js
@@ -0,0 +1,3 @@
+function capitalizer(params) {
+    return params.charAt(0).toLocaleUpperCase() + params.slice(1)
+}
