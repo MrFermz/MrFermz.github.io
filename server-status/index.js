52b42b7bcd088b8fdedfcd2c4032976794031664
MrFermz <Mr.Fermz@hotmail.com>
2020-08-20 19:58:25 +0700

    update MOTD clean

--- server-status/index.js
+++ server-status/index.js
@@ -5,8 +5,11 @@ if (localStorage.getItem('skinType')) {
     localStorage.setItem('skinType', SKIN_TYPE)
 }
 
+var colorCode = ['§0', '§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§a', '§b', '§c', '§d', '§e', '§f', '§k', '§l', '§m', '§n', '§o', '§r']
+
 
 async function onLoad() {
+    let result = ''
     // load server status
     let statProtocol = config.statusServer.protocol
     let statHost = config.statusServer.host
@@ -15,19 +18,24 @@ async function onLoad() {
     let statReq = `${statProtocol}://${statHost}/${statUrl}/${statIp}`
     let stat = await queriesGET(statReq)
     // let timer = stat.cache.ttl * 1000
-    console.log(stat)
+    let motd = stat.description.text
+
+    // console.log(stat)
     let options = [
         { id: 'head2d', label: 'head 2d' },
         { id: 'head3d', label: 'head 3d' },
         { id: 'body3d', label: 'body 3d' },
     ]
 
+    for (let i = 0; i < motd.length; i++) {
+        if (motd[i] === '§') motd = motd.replace(motd[i] + motd[i + 1], ' ')
+    }
+    result = motd.replace(/ +/g, ' ').trim()
+
 
     let display = ''
     if (stat.online) {
-        // let motd = stat.description.text.split('-')
-        let motd = stat.description.text
-        motd = `-${motd}-`
+        motd = `${result}`
         display = `
         <div>[${stat.online ? 'online' : 'offline'}]</div>
         <div>IP: ${statIp}</div>
@@ -35,42 +43,45 @@ async function onLoad() {
         <div>Version: ${stat.version.name}</div>
         <div>Players: ${stat.players.online} / ${stat.players.max}</div>
         <br>
-        <div>
+        ${stat.players.sample
+                ? `
+            <div>
             <label for="select-type">Choose display: </label>
             <select class="select-type" id="select-type">
                 ${options.map((ele, i) => {
-            return (`
+                    return (`
                     <option value="${ele.id}">${ele.label}</option>    
                 `)
-        }).join('')}
+                }).join('')}
             </select>
         </div>
+        ` : ``}
 
         <div class="avatar-container">
         ${stat.players.sample && SKIN_TYPE == 'head2d' ? stat.players.sample.map((ele, i) => {
-            return (`
+                    return (`
                 <div class="avatar-wrapper">
                     <div class="avatar-name">${ele.name}</div>
                     <div class="avatar"><img src="https://crafatar.com/avatars/${ele.id}"></div>
                 </div>
             `)
-        }).join('') : ``}
+                }).join('') : ``}
         ${stat.players.sample && SKIN_TYPE == 'head3d' ? stat.players.sample.map((ele, i) => {
-            return (`
+                    return (`
                 <div class="avatar-wrapper">
                     <div class="avatar-name">${ele.name}</div>
                     <div class="avatar"><img src="https://crafatar.com/renders/head/${ele.id}?overlay=helm"></div>
                 </div>
             `)
-        }).join('') : ``}
+                }).join('') : ``}
         ${stat.players.sample && SKIN_TYPE == 'body3d' ? stat.players.sample.map((ele, i) => {
-            return (`
+                    return (`
                 <div class="avatar-wrapper">
                     <div class="avatar-name">${ele.name}</div>
                     <div class="avatar"><img src="https://crafatar.com/renders/body/${ele.id}?overlay"></div>
                 </div>
             `)
-        }).join('') : ``}
+                }).join('') : ``}
         </div >`
     } else {
         display = `
